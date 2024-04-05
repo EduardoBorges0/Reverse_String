822883e809561872fa7bce30438a5f3607a19d5f
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-04-05 17:16:53 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite aqui a palavra ou frase que deseja inverter:");
+        String word = sc.nextLine();
+        char[] letters = word.toCharArray();
+
+        StringBuilder reversedWord = new StringBuilder();
+        for (int i = letters.length - 1; i >= 0; i--) {
+            reversedWord.append(letters[i]);
+        }
+
+        System.out.println(reversedWord.toString());
+        sc.close();
+
     }
 }
